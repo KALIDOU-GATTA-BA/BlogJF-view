e43957d35201cc8c6dbfc572b056f2759a7546a4
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-10-01 01:52:12 +0200

    Update viewUpdateSide.php

--- backend/viewUpdateSide.php
+++ backend/viewUpdateSide.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
     <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
     	<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
         <script>tinymce.init({ selector:'textarea' });</script>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-            <link href="public/css/style.css" rel="stylesheet" /> 
+            <link href="../../public/css/style.css" rel="stylesheet" /> 
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
     	<header>
-    		<img src="public/images/alaska.jpeg">
+    		<img src="../../public/images/alaskaBack.jpg">
     	</header><br>
     	       <form action="../../index.php?action=confirmUpdate&amp;id=<?= $post['id'] ?>" method="post">
                 <div class="container">
+                  <label> Le titre du chapitre: <br></label>
                   <input type="text" rows="2" cols="2" name="tempChapterTitle" value="<?= htmlspecialchars($post['title']) ?>"><br><br>
                         <div class="form-group">
+                          <label> Le contenu du chapitre: <br></label>
                           <textarea class="form-control" rows="25" id="comment" name="tempChapterContent">
                             <?= nl2br(htmlspecialchars($post['content'])) ?> 
                           </textarea>
