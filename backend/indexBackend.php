e9f43fe954b0e1819ad828e8f6c6e3296075f26c
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-10-06 15:09:43 +0200

    Update indexBackend.php

--- backend/indexBackend.php
+++ backend/indexBackend.php
@@ -1,100 +1,125 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-            <link href="../../public/css/style.css" rel="stylesheet" /> 
-            <link href="../../public/css/styleBackend.css" rel="stylesheet" /> 
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    </head>
-    <body>
-        <header>
-            <img src="../../public/images/alaska.jpeg"><br><br>
-            <a href='../../controller/principale.php?deconnexion=true'><span class="dec"><button type="button" class="btn btn-danger">Déconnexion</button></span></a><br> <br> <br>
-            <a href="viewAddChapter.php"><em><button type="button" class="btn btn-primary btn-xs"><span class="glyphicon">&#xe127;</span> Ajouter un  chapitre</button></em></a>
-        </header><br>
-    
-        <div class="container">
-            <div class="jumbotron">GESTION DES CHAPITRES </div>
-                <?php                                                     
-                $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
-                $posts= $db->query('SELECT title, id from posts');
-                while ($data = $posts->fetch())
-                {
+ <!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="../../public/css/styleBackOff.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-12 col-md-12 col-lg-12 col-xs-12 col-xl-12">
+			<header>
+				<img src="../../public/images/alaskaBack.jpg" class="rounded" alt="Frog"/>
+			</header>
+		</div>
+	</div>
+</div><br>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12 col-md-12 col-lg-12 col-xs-12 col-xl-12">
+			<a href="../../controller/mainForLogin.php?deconnexion=true"><strong><span class="dec">Deconnexion</span></strong></a><br><br><br>
+			<div class="chapterManager">
+				<em><strong>GESTION DES CHAPITRES</strong></em>
+			</diV>
+			<br><br><br>
+			<table class="table table-dark table-hover">
+				<thead>
+					  <tr>
+						<th style="text-align:left;">Chapitres</th>
+						<th style="text-align:left;">Commentaires</th>
+						<th style="text-align:center;">Modifier</th>
+						<th style="text-align:right;">Supprimer</th>
+					  </tr>
+				</thead>
+					  	<?php                                                     
+                		$db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+                		$posts= $db->query('SELECT title, id, countComment from posts');
+                		while ($data = $posts->fetch()){ 
+                		?>
+                		<tbody>
+						  	<tr>
+						  	<td style="color:blue;"><?= htmlspecialchars($data['title']) ?></td>
+							<td style="text-align:left;"><?php  echo $data['countComment']; ?></td>
+							<td style="text-align:center;"><a href="../../index.php?action=update&amp;id=<?= $data['id'] ?>"><span class="glyphicon glyphicon-pencil" style="text-align:center;"></span></a></td>
+							<td style="text-align:right;"><a href="../../index.php?action=delete&amp;id=<?= $data['id'] ?>" onclick="return(confirm('Etes-vous sûr de vouloir supprimer ce chapitre?'));" ><span class="glyphicon glyphicon-trash"></span></a></td>
+							</tr>
+						</tbody>
+						<?php
+                    	}
+                    	$posts->closeCursor();
+                    	?>	  
+			</table>
+			<a href="viewAddChapter.php"><em><button type="button" class="btn btn-primary btn-xs"><span class="glyphicon">&#xe127;</span> Ajouter un chapitre</button></em></a>
+			<div class="commentManager">
+				<em><strong>GESTION DES COMMENTAIRES</strong></em>
+			</diV><br><br>
+			<div class="notReadCommentManager">
+				COMMENTAIRES NON LUS
+			</diV><br><br><br>	
+			<table class="table table-dark table-hover">
+				<thead>
+					  <tr class="table-dark">
+						<th>Chapitre</th>
+						<th style="text-align:center;">Date du commentaire</th>
+						<th style="text-align:right;">Commentaire</th>
+					  </tr>
+				</thead> 
+					   	<?php                                                     
+                        $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+                        $notRead= $db->query('SELECT SUBSTRING(comment, 1,20) AS cmt, post_id, id, comment_date from comments where notReadComment=1');
+                        while ($notReadComment = $notRead->fetch()){ 
+                        $postId=$notReadComment['post_id'];
+                        $chapter= $db->query("SELECT title from posts where id=$postId");
+                        $thisChapter = $chapter->fetch();?>
+                        <tbody>
+	                        <tr class="table-dark">
+							<td style="color:blue;"><?= $thisChapter['title']?></td>
+							<td  style="color:blue;text-align:center;"><?= $notReadComment['comment_date'] ?></td>
+							<td  style="color:indigo;text-align:right;"><a href="../../index.php?action=notReadComment&amp;id=<?= $notReadComment['post_id'] ?>&amp;idC=<?= $notReadComment['id'] ?>"><b><?php echo $notReadComment['cmt'] .'[...]'.'<b></a>' ?></td>
+							 </tr> 
+						 </tbody>
+						 <?php 
+						 }
+	                     $posts->closeCursor();
+	                     ?> 
+			</table>
+			<div class="reportedCommentManager">
+				COMMENTAIRES SIGNALES
+			</diV><br><br><br>
+			<table class="table table-dark table-hover">
+				<thead>
+					  <tr class="table-dark">
+						<th>Auteur</th>
+						<th style="color:black;text-align:center;">Commentaire</th>
+						<th style="color:black;text-align:right;">Modération</th>
+					  </tr>
+				</thead>
+					<?php                                                     
+	                $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+	                $reported= $db->query('SELECT author, id, post_id, SUBSTRING(comment, 1,20) AS cmt from comments where reported=1');
+	                while ($report = $reported->fetch()){?>    	
+				<tbody>
+					   <tr class="table-dark">
+						<td style="color:blue;"><?=$report['author']?></td>
+						<td  style="color:blue;text-align:center;"><a href="../../index.php?action=notReadComment&amp;id=<?= $report['post_id'] ?>&amp;idC=<?= $report['id'] ?>"><?=$report['cmt'].' [...]'?></a></td>
+						<td style="word-spacing:8px;text-align:right;""><a href="../../index.php?action=deleteReportedComment&amp;id=<?= $report['id'] ?>"><span class="glyphicon glyphicon-trash" style="color:red;"></span></a> 
+							<a href="../../index.php?action=reportedCommentsManager&amp;id=<?= $report['post_id'] ?>&amp;idC=<?= $report['id'] ?>"><span class="glyphicon glyphicon-send" style="color:green; font-size:24px;"></span></a></td>
+					  </tr>
+				</tbody>
+				<?php 
+				}
                 ?>
-                <div class="row">
-                    <h3>
-                        <table style="width:100%">
-                            <tr>
-                                <th><?= htmlspecialchars($data['title']) ?></th>                       
-                    </h3>
-                                    <p>     
-                                        <th colspan="2"> 
-                                            <button><a href="../../index.php?action=post&amp;id=<?= $data['id'] ?>"><span class="glyphicon glyphicon-eye-open"></span>Voir</a></buttton>
-                                        </th><br><br> <th colspan="2">                 
-                                                    <a href="../../index.php?action=update&amp;id=<?= $data['id'] ?>"><button><span class="glyphicon glyphicon-pencil"></span> Modifier</button></a>   
-                                                </th> 
-                                        <th colspan="2"> 
-                                            <button><a href="../../index.php?action=delete&amp;id=<?= $data['id'] ?>" onclick="return(confirm('Etes-vous sûr de vouloir supprimer ce chapitre?'));" ><span class="glyphicon glyphicon-trash"></span>Supprimer</a></buttton>
-                                        </th><br><br>
-                                    </p>
-                            </tr>  
-                        </table></div>
-                    <?php
-                    }
-                    $posts->closeCursor();
-                    ?>
-                    <br><br><br><br><br><br>
-                    <div class="jumbotron">GESTION DES COMMENTAIRES</div>
-                </div>
-                <div class="container">
-                    <div class="panel-group">
-                        <div class="panel panel-success">
-                                <div class="panel-heading">
-                                    <a href="../../notReadComment.php">Commentaires non lus</a> 
-                                </div>
-                                    
-                                <?php                                                     
-                                $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
-                                $notRead= $db->query('SELECT comment, post_id, comment_date from comments where notReadComment=1');
-                                while ($notReadComment = $notRead->fetch()){ 
-                                    $postId=$notReadComment['post_id'];
-                                    $chapter= $db->query("SELECT title from posts where id=$postId");
-                                    $thisChapter = $chapter->fetch();
-                                    echo "Nouveau commentaire sur le chapitre: " .'<strong>'.$thisChapter['title'] .'</strong>, posté le: <strong>'.$notReadComment['comment_date'] .'</strong><br>';?>
-                                <a href="../../index.php?action=notReadComment&amp;id=<?= $notReadComment['post_id'] ?>">
-                                    <b><?php echo $notReadComment['comment'] .'<br><br></b></a>';
-
-                                }?>
-
-                        </div></br></br>
-                            <div class="panel panel-danger">
-                                <div class="panel-heading">
-                                    COMMENTAIRES SIGNALÉS  
-                                </div>
-                            </div>
-                        </div>
-                            <table>
-                            <tr> <th>Auteur</th><th>Commentaire</th><th>Date du commentaire</th><th>Chapitre commenté</th></tr>
-                            <?php                                                     
-                            $db = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
-                            $reported= $db->query('SELECT author, comment, comment_date from comments where reported=1');
-                            while ($report = $reported->fetch()){?>
-                            
-                                
-                                <tr> <td><?=$report['author']?></td><td><?=$report['comment']?></td><td><?=$report['comment_date']?></td><td><?=$thisChapter['title']?></td></tr>
-                            </table>
-                            
-                        <?php }
-                            ?>
-                    
-            
-            
-        </div>
-    </body> <br><br><br><br>
+			</table>
+		</div>
+	</div>
+</div>
+</body><br><br><br><br>
     <footer>
         <?php require_once("footerBackend.php");?>
     </footer>
-</html>
+</html> 
