9438dacb516506cd471a44dfc564d7fd180ca354
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-10-22 23:05:07 +0200

    Update viewUpdateSide.php

--- backend/viewUpdateSide.php
+++ backend/viewUpdateSide.php
@@ -3,8 +3,11 @@
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8">
     	<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
-        <script>tinymce.init({ selector:'textarea' });</script>
-
+        <script>
+tinymce.init({ selector:'textarea',
+                entity_encoding : "raw", encoding: "UTF-8" });
+</script>
+ 
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
             <link href="../../public/css/style.css" rel="stylesheet" /> 
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -30,9 +33,5 @@
               <a href="view/backend/indexBackend.php">
                 <button type="button" class="btn btn-outline-danger">Annuler</button><br><br> 
               </a>
-              
     </body>
-    <footer>
-      <?php require_once ("footerBackend.php"); ?>
-    </footer>
-</html>
+</html>	
