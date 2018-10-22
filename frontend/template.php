76f11cada15b27937ad6d0adc29aee85ceac43fc
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-10-22 23:00:16 +0200

    Update template.php

--- frontend/template.php
+++ frontend/template.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
     <head>
+        <link rel="icon" href="public/images/jf.ico" />
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?= $title ?></title>
@@ -10,6 +11,19 @@
         <link href="public/css/style.css" rel="stylesheet" />
     </head>
     <body>
-        <?= $content ?>
-    </body>
+    <header>  
+                 <strong>
+                   <a href="http://kalidougattaba.fr/"><span class="glyphicon">&#xe021; Accueil</span></a> <br>
+                   <a href="controller/contact.php"><span class="glyphicon">&#x2709; Contact</span></a> </br>
+                  <a href="controller/login.php"><span class="glyphicon glyphicon-user"> Administration</span></a>
+                </strong> 
+    </header>
+      <div class="content">  
+        <?= $content ?> 
+      </div><br><br>
+<div class="foot">
+Copyright 2018 <br><strong>Kalidou Gatta BA</strong> </br>
+            Tous droits réservés </br></br></br>
+</div>     
+</body>
 </html>
