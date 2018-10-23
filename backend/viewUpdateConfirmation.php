95a9ed88badc24f54d6b0515c617e3d93b5f7bbf
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-10-23 11:58:41 +0200

    Update viewUpdateConfirmation.php

--- backend/viewUpdateConfirmation.php
+++ backend/viewUpdateConfirmation.php
@@ -3,12 +3,11 @@
         <head>
                 <meta charset="utf-8" />
                 <link href="bootstrap.min.css" rel="stylesheet" />
-                <link href="../../public/css/style.css" rel="stylesheet" /> 
-                <link href="../../public/css/styleBackend.css" rel="stylesheet" /> 
+                <link href="../../public/css/styleBackOff.css" rel="stylesheet" /> 
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         </head>
         <body>
-                <div class="updateChapter"> Le chapitre a bien été mis à jour !</div><i style="font-size:254px" class="fa">&#xf118;</i><br/>
+                <strong>Le chapitre a bien été mis à jour !</strong>
         	<p><a href="../view/backend/indexBackend.php"><i style="font-size:254px" class="fa">&#xf112;</i></a></p>
         </body>
 </html>
